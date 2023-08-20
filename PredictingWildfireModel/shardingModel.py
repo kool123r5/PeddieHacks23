161204a0b3bef5727b6526e30e68758a355f5876
Kushal <74308033+kool123r5@users.noreply.github.com>
2023-08-20 11:05:29 +0530

    Update shardingModel.py

--- PredictingWildfireModel/shardingModel.py
+++ PredictingWildfireModel/shardingModel.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorflowjs as tfjs
 
-model = tf.keras.models.load_model('/Users/kushalb/Documents/VSCode/PeddieHacks2023/firePredict.h5')
-tfjs.converters.save_keras_model(model, '/Users/kushalb/Downloads/predictingFireModel')
+model = tf.keras.models.load_model('path/to/the/predicting/fire/model.h5')
+tfjs.converters.save_keras_model(model, 'path/to/my/folder')
 
