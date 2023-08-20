b6c712cffbd5a498c623f299e40466b3ac099499
Kushal <74308033+kool123r5@users.noreply.github.com>
2023-08-20 10:07:47 +0530

    Update testModel.py

--- PredictingWildfireModel/testModel.py
+++ PredictingWildfireModel/testModel.py
@@ -3,7 +3,7 @@ from PIL import Image
 import os
 import matplotlib.pyplot as plt
 
-model = tf.keras.models.load_model('/Users/kushalb/Documents/VSCode/PeddieHacks2023/firePredict.h5')
+model = tf.keras.models.load_model('path/to/model/here.h5')
 
 # Function to predict class using the loaded model
 def predict_class(image_path):
@@ -22,11 +22,11 @@ def predict_class(image_path):
 
 # count = 0
 # total = 0
-# for file in os.listdir('/Users/kushalb/Downloads/fireData/test/wildfire'):
+# for file in os.listdir('same/image/to/test'):
 #     if total == 1000:
 #         break
 #     total = total + 1
-#     predicted_class = predict_class(os.path.join('/Users/kushalb/Downloads/fireData/test/wildfire', file))
+#     predicted_class = predict_class(os.path.join('heres/an/image/to/test', file))
 #     if predicted_class == 'Wildfire':
 #         count = count + 1
 
